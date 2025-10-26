f6cd73c81eacb44f89f82ebee88921b26490d3dd
prasanna <kutcharlapatiprasanna@gmail.com>
2025-10-26 20:42:49 +0530

    canvas

---
+++ canvasproject/freehand.js
@@ -0,0 +1,37 @@
+const pencil = document.getElementById("pencil");
+let isPencilActive = false //initially the pencil is inactive
+
+ const coloPicker = document.getElementById("color-picker")
+
+ coloPicker.addEventListener("change",()=>{
+   drawingColor = coloPicker.value ;
+ })
+
+function onpencilclick(){
+    pencil.classList.toggle("active");
+    isPencilActive=!isPencilActive; //enabling the drawing
+    //ispencilActive = !false = true;
+    if(isPencilActive){
+        // console.log(coloPicker.value)
+        // drawingColor=coloPicker.value ;
+         canvas.style.cursor="crosshair";
+        canvas.addEventListener("mousedown",onmousedown)
+    }
+    else{
+        canvas.style.cursor="auto";
+         canvas.removeEventListener("mousedown",onmousedown)
+    }
+}
+
+
+
+pencil.addEventListener("click",onpencilclick);
+
+//toggle means ::if class name is active you have to remove othrwise add
+
+// !false: not of false means the opposite of false, which is true
+// ! → means NOT (logical negation operator).
+
+// false → means something that is not true.
+
+// !false → means not false, which becomes true.
--- canvasproject/script.js
+++ canvasproject/script.js
@@ -1,105 +1,177 @@
-//legenda 
-// 1.making a free hand drawings.and some shapes as well
 
+// second time 
+//making a free hand drawing and some shapes as well
+
+// canvas has exclusively one method that is getcontext for this get context 
 
 const canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-
-
-// firstly for drawing a line
-
-// first we need to begin the path
-
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
 const c = canvas.getContext("2d");
- c.beginPath();
+/* c.beginPath();
 c.moveTo(200,300);
-c.lineTo(400,100);
-c.strokeStyle="red";
-c.lineWidth="10";
-c.globalAlpha=0.2;
-c.stroke();//static function draws the line with black clor from p1 to p2
-c.closePath();
- 
+c.lineTo (400,100);
+c.strokeStyle = "blue";
+c.lineWidth = 2 ;
+c.globalAlpha = 1;
+c.stroke();
+c.closePath() */
 
-// hard coded lines
 
-function multplelines(p1,p2,color,thickness){
-   c.beginPath();
-   c.moveTo(p1.x,p1.y);
-   c.lineTo(p1.x,p2.y);
-   c.strokeStyle = color;
-   c.lineWidth = thickness;
-   c.stroke();
-   c.closePath();
-
+//multiple lines
+/* 
+function drawline(p1,p2,color, thickness){
+ c.beginPath();
+c.moveTo(p1.x,p1.y);
+c.lineTo (p2.x,p2.y);
+c.strokeStyle = color;
+c.lineWidth = thickness;
+// c.global 
+c.stroke();
+c.closePath()
 }
-multplelines({x:100,y:50},{x:90,y:200},"red",1);
-multplelines({x:10,y:950},{x:900,y:209},"red",1); 
+drawline({x:100,y:50},{x:90,y:200},"violet",3)
+drawline({x:50,y:150},{x:300,y:40},"green",10)
+drawline({x:500,y:500},{x:600,y:600},"blue",4) */
 
-// dynamic code 
 
-function onMouseDown(event) {
-    console.log(event);
-    let { clientX:x, clientY:y} = event;
+// dynamic
+
+//default parameter function
+/* function call(x,y=90){
+    console.log(x,y);
+}
+call(3) */
+
+// user is able to make a draw on the canvas by using mouse :mouseup,mousedown whwerever we want .
+// mousedown means:start the mouse 
+// mouseup:end the mouse
+// you’re trying to draw a line on a <canvas> by clicking(mousedown) and releasing(mouseup) the mouse.
+
+/* function onmousedown(event){
+    // let positon = event.clientX,clientY;
+    let {clientX,clientY}=event;
+    // {clientX,clientY} is position where use makes the mouse down
     c.beginPath();
-    c.moveTo(x, y);
-    c.lineWidth = 2;
+    c.moveTo(clientX,clientY);//starts the line at this position
+    c.lineWidth=5;
     c.strokeStyle = "blue";
+    
 }
 
-function onMouseUp(event) {
-    let { clientX, clientY } = event;
-    console.log(clientX, clientY);
-    c.lineTo(clientX, clientY);
+
+function onmouseup(event){
+    // let positon = event.clientX,clientY;
+    let {clientX,clientY}=event;
+    c.lineTo(clientX,clientY);
     c.stroke();
     c.closePath();
 }
 
-canvas.addEventListener("mousedown", onMouseDown);
-canvas.addEventListener("mouseup", onMouseUp);
+
+canvas.addEventListener("mousedown",onmousedown);
+canvas.addEventListener("mouseup",onmouseup); */
 
 
+// continuous lines
 
-// CanvasRenderingContext2D object.
-// these are methods execpt strokeStyle lineWidth because these two properties for stroke we have to set for values .
-// continues lines.
-
-c.beginPath();
+/* c.beginPath();
 c.strokeStyle="red";
-c.lineWidth = 3;
-c.moveTo(100,200);
+c.moveTo(100,100);
 c.lineTo(300,100);
+c.lineWidth = 10;
 c.stroke();
-c.closePath();
+c.lineTo(350,300);
+c.strokeStyle="blue"
+c.stroke(); */
+
+/* c.beginPath();
+c.strokeStyle="red";
+c.moveTo(100,100);
+c.lineTo(300,100);
+c.lineWidth = 10;
+c.stroke();
 c.beginPath();
-c.moveTo(300,100);
-c.lineTo(20,20);
-c.lineWidth = 2;
-c.strokeStyle = "green";
-c.stroke();
+c.moveTo(300,100)
+c.lineTo(350,300);
+c.strokeStyle="blue";
+c.lineWidth=2
+c.stroke(); */
 
 
-//free hand drawing
-canvas.addEventListener("mousedown",onMouseDown);
-let previousPosition = null;
+// mousemove
+// its like continuosly calling function
+// canvas.addEventListener("mousemove",(e)=>{
+//     console.log(e.clientX,e.clientY); 
+// })
 
 
-function onMouseDown(event){
+// free hand drawing
+
+// mousemove event mousedown event start atyna tharuvaatha trigger avvaaali and mousup ener avvagaane stop.and after remove mousemove only mosedown tharuvaatha mosemove and mouseup no mosemovmove.
+// we need to remove mouse move listner after mouseup
+
+/* canvas.addEventListener("mousedown",onmousedown)
+let previousvalue = null;
+function onmousedown(){
     console.log("mouse down");
-    canvas.addEventListener("mousemove",onMouseMove);
-    canvas.addEventListener("mouseup",onMouseUp)
-
+    canvas.addEventListener("mousemove",onmousemove);
+    canvas.addEventListener("mouseup",onmouseup);
 }
-function onMouseMove(event){
+function onmousemove(){
     console.log("mousemove")
 }
-function onMouseUp(){
-    console.log("mouseup");
-    canvas.addEventListener("mousemove",onMouseMove);
+function onmouseup(){
+    console.log("mouse up");
+    // we need to remove mouse move listner after mouseup
+    canvas.removeEventListener("mousemove",onmousemove)
+
+} */
+
+// you guys can see positions actually for events when triggring 
+  /*   canvas.addEventListener("mousedown",onmousedown)
+let previousvalue = null;
+function onmousedown(e){
+    console.log("mouse down" , e.clientX,e.clientY);
+    canvas.addEventListener("mousemove",onmousemove);
+    canvas.addEventListener("mouseup",onmouseup);
+}
+function onmousemove(e){
+    console.log("mousemove" , e.clientX,e.clientY)
+}
+function onmouseup(e){
+    console.log("mouse up", e.clientX,e.clientY);
+      canvas.removeEventListener("mousemove",onmousemove)
+}
+ */
+// free hand now
+//for the first time  inide this previous position 
+//  canvas.addEventListener("mousedown",onmousedown)
+let drawingColor = "blue";
+// let preveiousposition = null;
+function onmousedown(e){
+     previouPosition = [e.clientX,e.clientY];
+//   c.stroke();
+    c.strokeStyle=drawingColor;
+    c.lineWidth=10
+    canvas.addEventListener("mousemove",onmousemove);
+    canvas.addEventListener("mouseup",onmouseup);
+}
+function onmousemove(e){
+   let currentposition = [e.clientX,e.clientY]
+   c.beginPath();
+   c.moveTo(...previouPosition);
+   c.lineTo(...currentposition);
+//    c.strokeStyle="blue";
+c.stroke();
+   c.closePath();
+   previouPosition=currentposition;
+
+}
+function onmouseup(e){
+    console.log("mouse up", e.clientX,e.clientY);
+      canvas.removeEventListener("mousemove",onmousemove)
 }
 
-
-
-
+// upon clicking the pencil 
